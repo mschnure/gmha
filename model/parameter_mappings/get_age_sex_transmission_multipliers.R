b25350191427735ed48778183f12f7921376278a
mschnure <mschnur3@jhmi.edu>
2025-01-28 17:22:14 -0500

    cleaning up age.sex transmission multipliers

---
+++ model/parameter_mappings/get_age_sex_transmission_multipliers.R
@@ -0,0 +1,47 @@
+source("model/parameter_mappings/kenya/age_sex_transmission_multipliers_kenya.R")
+source ("model/parameter_mappings/south_africa/age_sex_transmission_multipliers_south_africa.R")
+
+
+get.all.age.sex.transmission.multipliers = function(location){
+    # if(location!="Kenya")
+    #     print("Using Kenya-specific transmission multipliers by age and sex for now")
+    
+    rv = list()
+    
+    if(location=="Kenya"){
+        rv$FEMALE.AGE.MULTIPLIERS.2003 = get.all.transmission.multipliers.kenya(sex="female",year=2003)
+        rv$MALE.AGE.MULTIPLIERS.2003 = get.all.transmission.multipliers.kenya(sex="male",year=2003)
+        
+        rv$FEMALE.AGE.MULTIPLIERS.2008 = get.all.transmission.multipliers.kenya(sex="female",year=2008)
+        rv$MALE.AGE.MULTIPLIERS.2008 = get.all.transmission.multipliers.kenya(sex="male",year=2008)
+        
+        rv$FEMALE.AGE.MULTIPLIERS.2014 = get.all.transmission.multipliers.kenya(sex="female",year=2014)
+        rv$MALE.AGE.MULTIPLIERS.2014 = get.all.transmission.multipliers.kenya(sex="male",year=2014)
+        
+        rv        
+    } else if(location=="South Africa"){
+        rv$FEMALE.AGE.MULTIPLIERS.2003 = get.all.transmission.multipliers.south.africa(sex="female",year=2003)
+        rv$MALE.AGE.MULTIPLIERS.2003 = get.all.transmission.multipliers.south.africa(sex="male",year=2003)
+        
+        rv$FEMALE.AGE.MULTIPLIERS.2008 = get.all.transmission.multipliers.south.africa(sex="female",year=2003) # USING 2003 ESTIMATES
+        rv$MALE.AGE.MULTIPLIERS.2008 = get.all.transmission.multipliers.south.africa(sex="male",year=2003)
+        
+        rv$FEMALE.AGE.MULTIPLIERS.2014 = get.all.transmission.multipliers.south.africa(sex="female",year=2016) # USING 2003 ESTIMATES
+        rv$MALE.AGE.MULTIPLIERS.2014 = get.all.transmission.multipliers.south.africa(sex="male",year=2016)
+        
+        rv  
+    } else { # using kenya model for all other countries for now 
+        rv$FEMALE.AGE.MULTIPLIERS.2003 = get.all.transmission.multipliers.kenya(sex="female",year=2003)
+        rv$MALE.AGE.MULTIPLIERS.2003 = get.all.transmission.multipliers.kenya(sex="male",year=2003)
+        
+        rv$FEMALE.AGE.MULTIPLIERS.2008 = get.all.transmission.multipliers.kenya(sex="female",year=2008)
+        rv$MALE.AGE.MULTIPLIERS.2008 = get.all.transmission.multipliers.kenya(sex="male",year=2008)
+        
+        rv$FEMALE.AGE.MULTIPLIERS.2014 = get.all.transmission.multipliers.kenya(sex="female",year=2014)
+        rv$MALE.AGE.MULTIPLIERS.2014 = get.all.transmission.multipliers.kenya(sex="male",year=2014)
+        
+        rv  
+    }
+        
+
+}
--- model/parameter_mappings/south_africa/age_sex_transmission_multipliers_south_africa.R
+++ model/parameter_mappings/south_africa/age_sex_transmission_multipliers_south_africa.R
@@ -1,68 +1,109 @@
-# South African HIV Behavioural, Sero-status and Media (SABSSM) surveys
-# 2012: South Africa National HIV Prevalence, Incidence and Behaviour Survey: 
-# https://hsrc.ac.za/uploads/pageContent/4565/SABSSM%20IV%20LEO%20final.pdf
-# multiple sexual partners, figure 3.8
-# condom use last sex, figure 3.10
+# SA DHS 2016, Tables 12.2.1 and 12.2.2 - high-risk sex = multiple partners 
+# SA DHS 2003, Table 5.13 - high-risk sex = non-marital/non-cohabiting partner
 
-## HI Allison!
-
-# @Allison: Just to orient yourself to what you are building below, run this line:
-data = data.frame(
-    ages = c("15-24","25-49","50+"),
-    prop.multiple.partners=c(.230,.115,.075), # values come from the bar graph in figure 3.8, year 2002, male
-    condom.use.last.sex = c(.571,.267,.082)) # values come from the bar graph in figure 3.10, year 2002, male
-
-# then look at the "data" dataframe: 
-data
-
-# now, fill in the below function with all the dataframes we need 
-get.transmission.multipliers.SABSSM  = function(sex,
-                                                year){
-    if(sex=="male"){
-        if(year==2002){
+get.transmission.multipliers.DHS.south.africa  = function(sex,
+                                                          year){
+    if(sex=="female"){
+        if(year==2016){ # table 12.2.1
             data = data.frame(
-                ages = c("15-24","25-49","50+"),
-                prop.multiple.partners=c(.230,.115,.075), 
-                condom.use.last.sex = c(.571,.267,.082)) 
-        } else if(year==2005){
-            # fill in a similar data = data.frame... section for year 2005, male
-            
-            
-        } else if(year==2008){
-            # same for 2008, male
-            
-        } else if(year==2012){
-            # same for 2012, male
-            
-        } else stop("data for given year not available") # this statement stops the function if we try to access a year for which data are not available 
-    } else if(sex=="female"){
-        if(year==2002){
-            
-            # fill in the same as what you did above, but now for female 
-            
-        } else if(year==2005){
-            
+                ages = c("15-19","20-24","25-29","30-39","40-49"),
+                prop.high.risk.sex=c(.027,.066,.069,.049,.02), # 2+ partners in past 12 months 
+                condom.use.among.high.risk.sex = c(.548,.640,.633,.515,.485)) 
         } else if(year==2008){
             
-        } else if(year==2012){
             
-        } else stop("data for given year not available")
-    } else stop ("sex must be male or female")
+        } else if(year==2003){
+            data = data.frame( # table 5.13
+                ages = c("15-19","20-24","25-29","30-39","40-49"),
+                prop.high.risk.sex=c(.948,.846,.642,.421,.338), # high risk sex in past 12 month
+                condom.use.among.high.risk.sex = c(.492,.530,.455,.391,.380)) 
+            
+        }
+    } else if(sex=="male"){
+        if(year==2016){ # table 12.2.2
+            data = data.frame(
+                ages = c("15-19","20-24","25-29","30-39","40-49"),
+                prop.high.risk.sex=c(.157,.262,.198,.170,.072), # 2+ partners in past 12 months 
+                condom.use.among.high.risk.sex = c(.879,.630,.660,.555,.514))
+        } else if(year==2008){
+            
+        } else if(year==2003){
+            data = data.frame( # table 5.13
+                ages = c("15-19","20-24","25-29","30-39","40-49"),
+                prop.high.risk.sex=c(.991,.946,.836,.516,.252), # high risk sex in past 12 month
+                condom.use.among.high.risk.sex = c(.250,.277,.305,.299,.408)) 
+        }
+    }
     
-    data$prop.at.risk = data$prop.multiple.partners*(1-data$condom.use.last.sex)
-    data$multiplier = data$prop.at.risk/(data$prop.at.risk[data$ages=="25-49"])
+    data$prop.at.risk = data$prop.high.risk.sex*(1-data$condom.use.among.high.risk.sex)
+    data$multiplier = data$prop.at.risk/(data$prop.at.risk[data$ages=="30-39"])
     
-    # "rv" stands for "return value" - what we want the function to return when we call it 
     rv = array(data$multiplier,
                dim = length(data$ages),
                dimnames = list(data$ages))
     
-    # a function in R will always return the last line of code - so here, the function will return a call to the rv array we made
-    rv 
+    rv
 }
 
-# Once you've filled out the above sections, run the line above where you define the function (line 17), and then try calling the function yourself and see what you get
-# this will call the function and return the values for male, 2002 (which I filled in), but you can try out other sexes/years
-get.transmission.multipliers.SABSSM(sex = "male",
-                                    year = "2002")
+
+get.transmission.multipliers.older.cohort.south.africa = function(sex){
+    if(sex=="female"){
+        data = data.frame(
+            ages = c("40-49","50-59","60-69","70-79"),
+            prop.high.risk.sex=c(.015,.015,.003,.003), 
+            # data says 50s and 70s have 0% multiple partners; that won't work, so instead, using 40s value for 50s and 60s value for 70s
+            condom.use.among.high.risk.sex = c(.165,.102,0,0)) 
+    } else if(sex=="male"){
+        data = data.frame(
+            ages = c("40-49","50-59","60-69","70-79"),
+            prop.high.risk.sex=c(.195,.197,.123,.083),
+            condom.use.among.high.risk.sex = c(.2,.06,.056,.018)) 
+    }
+    
+    data$prop.at.risk = data$prop.high.risk.sex*(1-data$condom.use.among.high.risk.sex)
+    data$multiplier = data$prop.at.risk/(data$prop.at.risk[data$ages=="40-49"])
+    
+    rv = array(data$multiplier,
+               dim = length(data$ages),
+               dimnames = list(data$ages))
+    
+    rv
+    
+}
+
+
+get.all.transmission.multipliers.south.africa = function(sex,
+                                                         year){
+    multipliers = c(rep(0,length(get.age.brackets.in.range(lower = 0, upper = 15))),
+                    rep(get.transmission.multipliers.DHS.south.africa(sex=sex, year=year)["15-19"],
+                        length(get.age.brackets.in.range(lower = 15, upper = 20))),
+                    rep(get.transmission.multipliers.DHS.south.africa(sex=sex, year=year)["20-24"],
+                        length(get.age.brackets.in.range(lower = 20, upper = 25))),
+                    rep(get.transmission.multipliers.DHS.south.africa(sex=sex, year=year)["25-29"],
+                        length(get.age.brackets.in.range(lower = 25, upper = 30))),
+                    rep(get.transmission.multipliers.DHS.south.africa(sex=sex, year=year)["30-39"],
+                        length(get.age.brackets.in.range(lower = 30, upper = 40))),
+                    rep(get.transmission.multipliers.DHS.south.africa(sex=sex, year=year)["40-49"],
+                        length(get.age.brackets.in.range(lower = 40, upper = 50))),
+                    
+                    # multiply 40-49 from DHS by 50-59 multiplier from older cohort
+                    rep((get.transmission.multipliers.DHS.south.africa(sex=sex, year=year)["40-49"]*
+                             get.transmission.multipliers.older.cohort.south.africa(sex=sex)["50-59"]),
+                        length(get.age.brackets.in.range(lower = 50, upper = 60))), 
+                    
+                    # multiply 40-49 from DHS by 60-69 multiplier from older cohort
+                    rep((get.transmission.multipliers.DHS.south.africa(sex=sex, year=year)["40-49"]*
+                             get.transmission.multipliers.older.cohort.south.africa(sex=sex)["60-69"]),
+                        length(get.age.brackets.in.range(lower = 60, upper = 70))), 
+                    
+                    # multiply 40-49 from DHS by 70-79 multiplier from older cohort
+                    rep((get.transmission.multipliers.DHS.south.africa(sex=sex, year=year)["40-49"]*
+                             get.transmission.multipliers.older.cohort.south.africa(sex=sex)["70-79"]),
+                        length(get.age.brackets.in.range(lower = 70, upper = Inf)))
+    )
+    names(multipliers) = get.age.brackets.in.range(lower=0,upper=Inf)
+    multipliers
+}
+
+
 
