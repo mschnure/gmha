24679a136ccf83ea6c5602cc523b354fb00e9fb9
mschnure <mschnur3@jhmi.edu>
2025-02-06 16:10:09 -0500

    plotting engagement/suppression

--- model/test_run.R
+++ model/test_run.R
@@ -8,6 +8,8 @@ variable.parameters.thailand=get.default.parameters(location = "Thailand")
 variable.parameters.netherlands=get.default.parameters(location = "Netherlands")
 variable.parameters.france=get.default.parameters(location = "France")
 
+variable.parameters.south.africa["log.OR.engagement.slope"] = -0.1
+
 sim.kenya = run.model.for.parameters(location="Kenya",variable.parameters = variable.parameters.kenya)
 sim.south.africa = run.model.for.parameters(location="South Africa",variable.parameters = variable.parameters.south.africa)
 sim.cambodia = run.model.for.parameters(location="Cambodia",variable.parameters = variable.parameters.cambodia)
@@ -68,3 +70,15 @@ simplot(sim.kenya,
         #sim.france,
         years=c(1970:2020),
         data.types = "total.mortality", facet.by = 'age')
+
+simplot(#sim.kenya,
+        sim.south.africa, 
+        years=1980:2020, 
+        data.types='engagement', 
+        proportion=T)
+
+simplot(sim.kenya,
+        #sim.south.africa, 
+        years=1980:2020, 
+        data.types='suppression', 
+        proportion=T)
