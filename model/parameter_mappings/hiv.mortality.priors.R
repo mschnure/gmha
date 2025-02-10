e593045c89d1232eba078368cd4834f0eb6cd94e
mschnure <mschnur3@jhmi.edu>
2025-02-10 15:24:07 -0500

    new HIV mortality rates for each country

---
+++ model/parameter_mappings/hiv.mortality.priors.R
@@ -0,0 +1,28 @@
+LOCATION = "Kenya"
+
+# 1990 AIDS-related deaths/1990 prevalence
+hiv.mortality.1990 = DATA.MANAGER$hiv.mortality$year.location["1990",LOCATION]/
+    DATA.MANAGER$prevalence$year.location["1990",LOCATION]
+
+# 2005 AIDS-related deaths/1990 prevalence
+hiv.mortality.2005 = DATA.MANAGER$hiv.mortality$year.location["2005",LOCATION]/
+    DATA.MANAGER$prevalence$year.location["2005",LOCATION]
+
+# 2020 AIDS-related deaths/1990 prevalence
+hiv.mortality.2020 = DATA.MANAGER$hiv.mortality$year.location["2020",LOCATION]/
+    DATA.MANAGER$prevalence$year.location["2020",LOCATION]
+
+# Kenya
+hiv.mortality.1990 # 0.04057971
+hiv.mortality.2005 # 0.08125
+hiv.mortality.2020 # 0.02
+
+# South Africa
+hiv.mortality.1990 # 0.03030303
+hiv.mortality.2005 # 0.05306122
+hiv.mortality.2020 # 0.007432432
+
+# France
+hiv.mortality.1990 # 0.05357143 
+hiv.mortality.2005 # 0.002272727
+hiv.mortality.2020 # 0.003947368
