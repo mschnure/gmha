fa81cfd9a3bf7081f648e7f1cbc5a61367cf537a
Melissa Schnure <mschnur3@jh.edu>
2025-02-13 12:34:02 -0500

    reviewing france

--- helpers/review_france_mcmc_v2.R
+++ helpers/review_france_mcmc_v2.R
@@ -1,62 +1,70 @@
+load("mcmc_runs/mcmc_files/mcmc_france_2025-02-13.Rdata")
+simset.no.burn = extract.simset(mcmc,
+                        additional.burn=1,
+                        additional.thin=1) 
 
-load("mcmc_runs/simset_france_2025-02-11.Rdata")
-sim.mcmc = simset@simulations[[simset@n.sim]]
+load("~/gmha/mcmc_runs/simset_france_2025-02-12.Rdata")
+simset.old=simset
+params.first.old = simset.old@parameters[1,]
 
-simplot(sim.mcmc, 
-        years=1980:2030, 
-        facet.by='age', 
-        ages = MODEL.TO.SURVEILLANCE.AGE.MAPPING$`All ages`,
-        data.types='prevalence')
+load("~/gmha/mcmc_runs/simset_france_2025-02-13.Rdata")
+simset.new=simset
+params.first.new = simset.new@parameters[1,]
 
-params.mcmc = simset@parameters[simset@n.sim,]
-params.manual = params.mcmc
+params.manual = params.first.new
 
-cbind(params.manual)
-# params.manual["age.15.to.19.aging.factor"] = 1 #0.024593083
-# params.manual["age.20.to.24.aging.factor"] = 1 #0.233335955
-params.manual["age.25.to.50.aging.factor"] = 1 #0.798845362
+params.default = get.default.parameters("France")
+cbind(params.default[names(params.first.old)],params.first.old,
+      params.first.old/params.default[names(params.first.old)]) 
+cbind(params.default[names(params.first.new)],params.first.new,
+      params.manual,
+      params.first.new/params.default[names(params.first.new)]) 
 
-## MIGHT NEED TO FURTHER RESTRICT OR REMOVE THESE AGING FACTORS 
+# params.manual["log.OR.suppression.slope"] = 0
+# params.manual["log.OR.testing.intercept"] = 0
+# params.manual["log.OR.testing.slope"] = 0
+# params.manual["log.OR.engagement.slope"] = 0
+
+transmission.multipliers = names(params.manual[grepl("transmission.multiplier",names(params.manual))])
+
+params.manual[transmission.multipliers] = params.default[transmission.multipliers]
 
 sim.manual = run.model.for.parameters(variable.parameters = params.manual,
                                       end.year = 2040,
-                                      location = sim.mcmc$location)
+                                      location = simset@simulations[[1]]$location)
 
-simplot(sim.mcmc, 
-        sim.manual,
+sim.first = simset.no.burn@simulations[[1]]
+sim.last = simset.new@simulations[[simset.new@n.sim]]
+sim.old.last = simset.old@simulations[[simset.old@n.sim]]
+
+simplot(#sim.france, 
+        #sim.first,
+        sim.old.last,
+        sim.last,
+        #sim.manual,
         years=1980:2030, 
         facet.by='age', 
         #ages = MODEL.TO.SURVEILLANCE.AGE.MAPPING$`All ages`,
-        data.types='prevalence')
-
-simplot(sim.mcmc, 
-        sim.manual,
-        years=1980:2030, 
-        facet.by='age', 
-        # ages = MODEL.TO.SURVEILLANCE.AGE.MAPPING$`All ages`,
         data.types='incidence')
 
-plot.age.distribution(sim.manual,
-                      plot.limits=c(0,35000))
-
-simplot(sim.mcmc, 
-        sim.manual,
-        years = 1980:2030, 
-        data.types = "population",
-        facet.by='age')
+simplot(sim.old.last,
+        sim.last,
+        years=1980:2020, 
+        data.types='suppression', 
+        proportion=T)
 
 source("calibration/likelihood/individual_likelihoods.R") # make sure to change country to France before sourcing 
 likelihood.to.run = create.likelihood(parameters = create.model.parameters(location = "France"),
                                       location="France")
 
-exp(likelihood.to.run(sim.manual) - likelihood.to.run(sim.mcmc))
-exp(full.lik(sim.manual) - full.lik(sim.mcmc))
-exp(pop.lik(sim.manual) - pop.lik(sim.mcmc))
-exp(incidence.lik(sim.manual) - incidence.lik(sim.mcmc)) # this is worse; what if I removed 2x weight
-exp(prev.lik(sim.manual) - prev.lik(sim.mcmc))
-exp(aware.lik(sim.manual) - aware.lik(sim.mcmc)) # also worse 
-exp(eng.lik(sim.manual) - eng.lik(sim.mcmc))
-exp(supp.lik(sim.manual) - supp.lik(sim.mcmc)) 
-exp(hiv.mortality.lik(sim.manual) - hiv.mortality.lik(sim.mcmc))
-exp(aware.trend.lik(sim.manual) - aware.trend.lik(sim.mcmc))
-exp(total.mortality.lik(sim.manual) - total.mortality.lik(sim.mcmc)) 
+exp(likelihood.to.run(sim.last) - likelihood.to.run(sim.old.last))
+exp(full.lik(sim.last) - full.lik(sim.old.last))
+exp(pop.lik(sim.last) - pop.lik(sim.old.last))
+exp(incidence.lik(sim.last) - incidence.lik(sim.old.last))
+exp(prev.lik(sim.last) - prev.lik(sim.old.last))
+exp(aware.lik(sim.last) - aware.lik(sim.old.last)) 
+exp(eng.lik(sim.last) - eng.lik(sim.old.last))
+exp(supp.lik(sim.last) - supp.lik(sim.old.last)) 
+exp(hiv.mortality.lik(sim.last) - hiv.mortality.lik(sim.old.last))
+exp(aware.trend.lik(sim.last) - aware.trend.lik(sim.old.last))
+exp(total.mortality.lik(sim.last) - total.mortality.lik(sim.old.last)) 
