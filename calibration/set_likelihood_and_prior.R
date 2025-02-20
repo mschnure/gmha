df2538d8a0ba88feb06aa2cd9b0373dcaacb7dd5
mschnure <mschnur3@jhmi.edu>
2025-02-20 10:51:07 -0500

    back to original prior

--- calibration/set_likelihood_and_prior.R
+++ calibration/set_likelihood_and_prior.R
@@ -9,20 +9,20 @@ set.likelihood.and.prior.by.location = function(location){
                                               location="Kenya")
         prior = KENYA.PRIOR
         
-        # params.start.values = get.default.parameters(location = "Kenya")
-        # params.start.values = params.start.values[prior@var.names]
-        load("calibration/starting_values/2025_02_13_kenya_start_values.Rdata") 
-        params.start.values = params.start.values
+        params.start.values = get.default.parameters(location = "Kenya")
+        params.start.values = params.start.values[prior@var.names]
+        # load("calibration/starting_values/2025_02_13_kenya_start_values.Rdata") 
+        # params.start.values = params.start.values
         
     } else if(location=="South Africa"){
         likelihood.to.run = create.likelihood(parameters = create.model.parameters(location = "South Africa"),
                                              location="South Africa")
         prior = SOUTH.AFRICA.PRIOR
         
-        # params.start.values = get.default.parameters(location = "South Africa")
-        # params.start.values = params.start.values[prior@var.names]
-        load("calibration/starting_values/2025_02_13_south_africa_start_values.Rdata")
-        params.start.values = params.start.values
+        params.start.values = get.default.parameters(location = "South Africa")
+        params.start.values = params.start.values[prior@var.names]
+        # load("calibration/starting_values/2025_02_13_south_africa_start_values.Rdata")
+        # params.start.values = params.start.values
         
     } else if(location=="France"){
         likelihood.to.run = create.likelihood(parameters = create.model.parameters(location = "France"),
@@ -30,7 +30,7 @@ set.likelihood.and.prior.by.location = function(location){
         
         prior = FRANCE.PRIOR
         
-        params.start.values = get.default.parameters(location = "France") # 1/24/25 - reverting to original prior values 
+        params.start.values = get.default.parameters(location = "France") 
         params.start.values = params.start.values[prior@var.names]
         # load("calibration/starting_values/2025_01_17_france_start_values.Rdata") 
         # params.start.values = params.start.values
