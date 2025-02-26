080ebfa43565255d21088a5b00b222154510244f
Melissa Schnure <mschnur3@jh.edu>
2025-02-26 13:03:44 -0500

    x

--- calibration/starting_values/2025_02_26_set_starting_values_kenya.R
+++ calibration/starting_values/2025_02_26_set_starting_values_kenya.R
@@ -2,5 +2,9 @@ load("mcmc_runs/simset_kenya_2025-02-26.Rdata")
 simset.kenya = simset
 params.start.values = simset.kenya@parameters[simset.kenya@n.sim,] 
 
+params.start.values["age.25.to.49.hiv.mortality.multiplier.0"] = 1
+params.start.values["age.25.to.49.hiv.mortality.multiplier.1"] = 1
+params.start.values["age.25.to.49.hiv.mortality.multiplier.2"] = 1
+
 save(params.start.values,file=("calibration/starting_values/2025_02_26_kenya_start_values.Rdata"))
 
