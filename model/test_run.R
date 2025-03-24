2fa42bb6ecdaa392ad763c63c2f14fc5a80e23e5
mschnure <mschnur3@jhmi.edu>
2025-03-24 12:13:32 -0400

    documentation/scratch

--- model/test_run.R
+++ model/test_run.R
@@ -1,22 +1,23 @@
 source('model/run_systematic.R')
 
-#LOCATION = "South Africa"
-variable.parameters.kenya=get.default.parameters(location = "Kenya")
-variable.parameters.south.africa=get.default.parameters(location = "South Africa")
-variable.parameters.france=get.default.parameters(location = "France")
-variable.parameters.mozambique=get.default.parameters(location = "Mozambique")
+variable.parameters.tanzania=get.default.parameters(location = "Tanzania")
+# variable.parameters.kenya=get.default.parameters(location = "Kenya")
+# variable.parameters.south.africa=get.default.parameters(location = "South Africa")
+# variable.parameters.france=get.default.parameters(location = "France")
+# variable.parameters.mozambique=get.default.parameters(location = "Mozambique")
 #variable.parameters.thailand=get.default.parameters(location = "Thailand")
 #variable.parameters.cambodia=get.default.parameters(location = "Cambodia")
 
-#set.seed(1234)
-sim.mozambique = run.model.for.parameters(location="Mozambique",variable.parameters = variable.parameters.mozambique)
+sim.tanzania = run.model.for.parameters(location="Tanzania",variable.parameters = variable.parameters.tanzania)
+#sim.mozambique = run.model.for.parameters(location="Mozambique",variable.parameters = variable.parameters.mozambique)
 #sim.kenya = run.model.for.parameters(location="Kenya",variable.parameters = variable.parameters.kenya)
 #sim.south.africa = run.model.for.parameters(location="South Africa",variable.parameters = variable.parameters.south.africa)
 #sim.france = run.model.for.parameters(location="France",variable.parameters = variable.parameters.france)
 #sim.thailand = run.model.for.parameters(location="Thailand",variable.parameters = variable.parameters.thailand)
 #sim.cambodia = run.model.for.parameters(location="Cambodia",variable.parameters = variable.parameters.cambodia)
 
-simplot(sim.mozambique,
+simplot(#sim.tanzania,
+        #sim.mozambique,
         #sim.kenya,
         #sim.south.africa,
         #sim.france,
@@ -25,16 +26,20 @@ simplot(sim.mozambique,
         years=c(1970:2030),
         data.types = c("incidence","prevalence"))
 
-simplot(#sim.kenya,
+simplot(sim.tanzania,
+        #sim.mozambique,
+        #sim.kenya,
         #sim.south.africa,
         #sim.cambodia,
         #sim.thailand,
-        sim.france,
+        #sim.france,
         years=c(1980:2020),
         data.types = c("incidence"),
         facet.by = 'age')
 
-simplot(sim.kenya,
+simplot(sim.tanzania,
+        #sim.mozambique,
+        #sim.kenya,
         #sim.south.africa,
         #sim.cambodia,
         #sim.thailand,
@@ -44,23 +49,29 @@ simplot(sim.kenya,
         data.types = c("prevalence"),
         facet.by = 'age')
 
-simplot(#sim.kenya,
+simplot(sim.tanzania,
+        #sim.mozambique,
+        #sim.kenya,
         #sim.south.africa,
         #sim.cambodia,
         #sim.thailand,
-        sim.france,
+        #sim.france,
         years=c(1970:2020),
         data.types = "population")
  
-simplot(#sim.kenya,
+simplot(sim.tanzania,
+        #sim.mozambique,
+        #sim.kenya,
         #sim.south.africa,
         #sim.cambodia,
         #sim.thailand,
-        sim.france,
+        #sim.france,
         years=c(1970:2020),
         data.types = "population", facet.by = 'age')
 
-simplot(sim.kenya,
+simplot(sim.tanzania,
+        #sim.mozambique,
+        #sim.kenya,
         #sim.south.africa,
         #sim.cambodia,
         #sim.thailand,
@@ -68,14 +79,14 @@ simplot(sim.kenya,
         years=c(1970:2020),
         data.types = "total.mortality", facet.by = 'age')
 
-simplot(#sim.kenya,
-        sim.south.africa, 
-        years=1980:2020, 
-        data.types='engagement', 
+simplot(sim.tanzania,
+        years=2010:2030, 
+        data.types=c('awareness',"engagement","suppression"), 
         proportion=T)
 
-simplot(sim.kenya,
-        #sim.south.africa, 
-        years=1980:2020, 
-        data.types='suppression', 
+simplot(sim.tanzania,
+        years=2010:2030, 
+        data.types=c('awareness',"engagement","suppression"), 
+        facet.by=c('age','sex'), 
         proportion=T)
+
