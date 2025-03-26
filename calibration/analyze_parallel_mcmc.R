0a493c8d929a5cfd9b0f49f363d682d6b2f2c57c
mschnure <mschnur3@jhmi.edu>
2025-03-26 11:11:31 -0400

    reviewing mcmc

--- calibration/analyze_parallel_mcmc.R
+++ calibration/analyze_parallel_mcmc.R
@@ -1,66 +1,71 @@
+library("RColorBrewer")
+library("scales")
+library("ggsci")
 source("model/run_systematic.R")
 
-#load("mcmc_runs/simset_kenya_2025-02-27.Rdata")
-#load("mcmc_runs/simset_south_africa_2025-02-27.Rdata")
-#load("mcmc_runs/simset_france_2025-02-27.Rdata")
+# Load "all.results" object that has: 
+    # simset.list.full$no.int --> main simset, thinned to 200 sims and run to 2040
+    # full.results.array --> full array that has all outcomes for all years and all sims, by age/sex
+    # summary.results --> list with different summary results like median age, percent over X age, etc. 
+    # export.to.csv --> summary results formatted into an array that can be easily esported to a csv 
+load('cached/all.results_mozambique_2025-03-25.Rdata')
+
+simset = simset.list.full$no.int
 
 simplot(simset,
         years = 1980:2030)
 
-simplot(simset, #@simulations[[simset@n.sim]],
+simplot(simset, 
+        years=1980:2030, 
+        facet.by='age', 
+        #ages = MODEL.TO.SURVEILLANCE.AGE.MAPPING$`All ages`,
+        data.types='incidence')
+
+simplot(simset, 
+        years=1980:2030, 
+        facet.by='age', 
+        #ages = MODEL.TO.SURVEILLANCE.AGE.MAPPING$`All ages`,
+        data.types='prevalence')
+
+simplot(simset, 
+        years=1980:2030, 
+        facet.by=c('age'), 
+        data.types='hiv.mortality')
+
+simplot(simset, 
+        years=1980:2030, 
+        facet.by=c('age',"sex"), 
+        ages = "15+", 
+        data.types='incidence')
+
+simplot(simset, 
+        years=1980:2030, 
+        facet.by=c('age',"sex"), 
+        ages = "15+", 
+        data.types='prevalence')
+
+simplot(simset, 
+        years=1980:2030, 
+        facet.by=c('age',"sex"), 
+        ages = "15+", 
+        data.types='hiv.mortality')
+
+simplot(simset, 
         years = 1980:2030, 
         data.types = "population")
 
-simplot(simset, #@simulations[[simset@n.sim]],
+simplot(simset,
         years = 1980:2030, 
         data.types = "population",
         facet.by='age')
 
-simplot(simset, 
-        years=1980:2030, 
-        facet.by='age', 
-        #ages = MODEL.TO.SURVEILLANCE.AGE.MAPPING$`All ages`,
-        data.types='incidence')
-
-simplot(simset, 
-        years=1980:2030, 
-        facet.by='age', 
-        #ages = MODEL.TO.SURVEILLANCE.AGE.MAPPING$`All ages`,
-        data.types='prevalence')
-
-simplot(simset, 
-        years=1980:2030, 
-        facet.by=c('age'), 
-        #ages = MODEL.TO.SURVEILLANCE.AGE.MAPPING$`All ages`,
-        data.types='hiv.mortality')
-
-simplot(simset, 
-        years=1980:2030, 
-        facet.by=c('age',"sex"), 
-        ages = "15+", 
-        data.types='incidence')
-
-simplot(simset, 
-        years=1980:2030, 
-        facet.by=c('age',"sex"), 
-        ages = "15+", 
-        data.types='prevalence')
-
-simplot(simset, 
-        years=1980:2030, 
-        facet.by=c('age',"sex"), 
-        ages = "15+", 
-        data.types='hiv.mortality')
-
-
-simplot(simset, #@simulations[[simset@n.sim]],  
+simplot(simset,
         years=1980:2030, 
         data.types='total.mortality')
 
-simplot(simset, #@simulations[[simset@n.sim]],
+simplot(simset,
         years=1980:2030, 
         facet.by=c('age'), 
-        #sexes = "male",
         data.types='total.mortality')
 
 simplot(simset,
@@ -74,39 +79,39 @@ simplot(simset,
         facet.by=c('age','sex'), 
         proportion=T)
 
-simplot(simset, 
-        years=1980:2020, 
-        data.types='engagement', 
-        proportion=T)
-
-simplot(simset, 
-        years=1980:2020, 
-        facet.by=c('age','sex'), 
-        data.types='engagement', 
-        proportion=T)
-
-simplot(simset, 
-        years=1980:2020, 
-        data.types='suppression', 
-        proportion=T)
-
-simplot(simset, 
-        years=1980:2020, 
-        facet.by=c('age','sex'),
-        data.types='suppression', 
-        proportion=T)
-
-simplot(simset, 
-        years=1980:2030, 
-        data.types='awareness', 
-        proportion=T)
-
-simplot(simset, 
-        years=1980:2030, 
-        facet.by=c('age','sex'), 
-        data.types='awareness', 
-        proportion=T)
 
 
+# AGE DISTRIBUTION PLOT
+if(simset.no.int@simulations[[1]]$location=="Kenya")
+    plot.limit = 200000 
+if(simset.no.int@simulations[[1]]$location=="South Africa")
+    plot.limit = 1150000 
+if(simset.no.int@simulations[[1]]$location=="France")
+    plot.limit = 35000 
+if(simset.no.int@simulations[[1]]$location=="Mozambique")
+    plot.limit = 400000
 
+pal = c(brewer.pal(n=12,"Paired")[2],brewer.pal(n=12,"Paired")[5],brewer.pal(n=12,"Paired")[4]) 
+alpha = 0.8
+
+generate.age.distribution(full.results.array, 
+                          outcome="prevalence", 
+                          intervention.1 = "no.int",year.1="2025",
+                          intervention.2 = "no.int",year.2="2040",
+                          intervention.3 = "no.int",year.3="2040",
+                          percent=F,
+                          sexes = c("female","male"),
+                          plot.limits=c(0,plot.limit)) +
+    scale_fill_manual(labels = c("no.int/2025" = "2025",
+                                 "no.int/2040" = "Status quo, 2040",
+                                 "no.int/2040" = "Status quo, 2040"), 
+                      values=alpha(c("no.int/2025" = pal[1],
+                                     "no.int/2040" = pal[2], 
+                                     "no.int/2040" = pal[3]),alpha), 
+                      name=NULL) +
+    theme(text = element_text(size = 20),
+          axis.title.y = element_text(colour = "grey37"))+
+    labs(title = NULL,subtitle = NULL) +
+    ylab(label = "Number of people living with HIV")+
+    guides(x =  guide_axis(angle = 45))
 
