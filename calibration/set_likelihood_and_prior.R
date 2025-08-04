c4e9eb113c84a6502d9b730f5ba986cdecbdedd8
Melissa Schnure <mschnur3@jh.edu>
2025-08-04 10:38:39 -0400

    set starting values

--- calibration/set_likelihood_and_prior.R
+++ calibration/set_likelihood_and_prior.R
@@ -22,38 +22,52 @@ set.likelihood.and.prior.by.location = function(location){
     
     if(location=="Kenya"){
         prior = KENYA.PRIOR
-        #load("calibration/starting_values/2025_02_26_kenya_start_values.Rdata")
-        #params.start.values = params.start.values
+        load("calibration/starting_values/2025_08_04_kenya_start_values.Rdata")
+        params.start.values = params.start.values
     } else if(location=="South Africa"){
         prior = SOUTH.AFRICA.PRIOR
-        #load("calibration/starting_values/2025_02_26_south_africa_start_values.Rdata")
-        #params.start.values = params.start.values
+        load("calibration/starting_values/2025_08_04_south_africa_start_values.Rdata")
+        params.start.values = params.start.values
     } else if(location=="France"){
         prior = FRANCE.PRIOR
-        # load("calibration/starting_values/2025_06_19_france_start_values.Rdata")
-        # params.start.values = params.start.values
+        load("calibration/starting_values/2025_08_04_france_start_values.Rdata")
+        params.start.values = params.start.values
     } else if(location=="Mozambique"){
         prior = MOZAMBIQUE.PRIOR
+        load("calibration/starting_values/2025_08_04_mozambique_start_values.Rdata")
+        params.start.values = params.start.values
     } else if(location=="Tanzania"){
         prior = TANZANIA.PRIOR
+        load("calibration/starting_values/2025_08_04_tanzania_start_values.Rdata")
+        params.start.values = params.start.values
     } else if(location=="Uganda"){
         prior = UGANDA.PRIOR
+        load("calibration/starting_values/2025_08_04_uganda_start_values.Rdata")
+        params.start.values = params.start.values
     } else if(location=="Zambia"){
         prior = ZAMBIA.PRIOR
+        load("calibration/starting_values/2025_08_04_zambia_start_values.Rdata")
+        params.start.values = params.start.values
     } else if(location=="Zimbabwe"){
         prior = ZIMBABWE.PRIOR
+        load("calibration/starting_values/2025_08_04_zimbabwe_start_values.Rdata")
+        params.start.values = params.start.values
     } else if(location=="Malawi"){
         prior = MALAWI.PRIOR
+        load("calibration/starting_values/2025_08_04_malawi_start_values.Rdata")
+        params.start.values = params.start.values
     } else if(location=="Nigeria"){
         prior = NIGERIA.PRIOR
+        load("calibration/starting_values/2025_08_04_nigeria_start_values.Rdata")
+        params.start.values = params.start.values
     } else if(location=="Thailand"){
         prior = THAILAND.PRIOR
-        #load("calibration/starting_values/2025_07_25_thailand_start_values.Rdata")
-        #params.start.values = params.start.values
+        load("calibration/starting_values/2025_08_04_thailand_start_values.Rdata")
+        params.start.values = params.start.values
     } else if(location=="Cambodia"){
         prior = CAMBODIA.PRIOR
-        #load("calibration/starting_values/2025_07_25_cambodia_start_values.Rdata")
-        #params.start.values = params.start.values
+        load("calibration/starting_values/2025_08_04_cambodia_start_values.Rdata")
+        params.start.values = params.start.values
     } else if(location=="unaids.remainder"){
         prior = UNAIDS.REMAINDER.PRIOR
     } else if(location=="non.unaids.remainder"){
@@ -63,7 +77,7 @@ set.likelihood.and.prior.by.location = function(location){
         prior = UNAIDS.REMAINDER.PRIOR
         print("using UNAIDS remainder prior for all r1 models")
     } else stop("Only set up for Kenya, South Africa, France, Mozambique, Tanzania, 
-             Uganda, Zambia, Zimbabwe, Malawi, Nigeria, Thailand, Cambodia remainder models for now")
+             Uganda, Zambia, Zimbabwe, Malawi, Nigeria, Thailand, Cambodia, and remainder models for now")
     
     params.start.values = params.start.values[prior@var.names]
     
