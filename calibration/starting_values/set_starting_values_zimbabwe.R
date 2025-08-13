2a13a6d4c04b80cd45b31f32769166fbda7df963
Melissa Schnure <mschnur3@jh.edu>
2025-08-13 11:22:12 -0400

    starting values

--- calibration/starting_values/set_starting_values_zimbabwe.R
+++ calibration/starting_values/set_starting_values_zimbabwe.R
@@ -5,10 +5,13 @@ prior = ZIMBABWE.PRIOR
 
 
 # initial run of 100,000 with 1/8 weight
-load("mcmc_runs/simset_zimbabwe_2025-08-02.Rdata")
+  #load("mcmc_runs/simset_zimbabwe_2025-08-02.Rdata")
+# after that ^ initial run, ran 50,000 with 4x prevalence weight
+load("mcmc_runs/simset_zimbabwe_2025-08-13.Rdata")
 
 default.params =  get.default.parameters(location = "Zimbabwe") 
 params.start.values = simset@parameters[simset@n.sim,] 
 additional.params = setdiff(names(default.params),names(params.start.values))
 
-save(params.start.values,file=("calibration/starting_values/2025_08_04_zimbabwe_start_values.Rdata"))
+#save(params.start.values,file=("calibration/starting_values/2025_08_04_zimbabwe_start_values.Rdata"))
+save(params.start.values,file=("calibration/starting_values/2025_08_13_zimbabwe_start_values.Rdata"))
