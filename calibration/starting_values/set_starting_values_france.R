cc030581225059dcb808f0c5b7e1f81c6cca8051
Melissa Schnure <mschnur3@jh.edu>
2025-08-18 10:12:16 -0400

    remaining starting values

--- calibration/starting_values/set_starting_values_france.R
+++ calibration/starting_values/set_starting_values_france.R
@@ -5,11 +5,15 @@ prior = FRANCE.PRIOR
 
 
 # initial run of 100,000 with 1/8 weight
-load("mcmc_runs/simset_france_2025-07-28.Rdata")
+  #load("mcmc_runs/simset_france_2025-07-28.Rdata")
+# after that ^ initial run (stopped at 79%), ran 50,000 with 4x prevalence weight
+load("mcmc_runs/simset_france_2025-08-13.Rdata")
 
 default.params =  get.default.parameters(location = "France") 
 params.start.values = simset@parameters[simset@n.sim,] 
 additional.params = setdiff(names(default.params),names(params.start.values))
 
-save(params.start.values,file=("calibration/starting_values/2025_08_04_france_start_values.Rdata"))
+#save(params.start.values,file=("calibration/starting_values/2025_08_04_france_start_values.Rdata"))
+save(params.start.values,file=("calibration/starting_values/2025_08_13_france_start_values.Rdata"))
+
 
--- calibration/starting_values/set_starting_values_nigeria.R
+++ calibration/starting_values/set_starting_values_nigeria.R
@@ -5,10 +5,13 @@ prior = NIGERIA.PRIOR
 
 
 # initial run of 100,000 with 1/8 weight
-load("mcmc_runs/simset_nigeria_2025-07-31.Rdata")
+  #load("mcmc_runs/simset_nigeria_2025-07-31.Rdata")
+# after that ^ initial run (stopped at 79%), ran 50,000 with 4x prevalence weight
+load("mcmc_runs/simset_nigeria_2025-08-13.Rdata")
 
 default.params =  get.default.parameters(location = "Nigeria") 
 params.start.values = simset@parameters[simset@n.sim,] 
 additional.params = setdiff(names(default.params),names(params.start.values))
 
-save(params.start.values,file=("calibration/starting_values/2025_08_04_nigeria_start_values.Rdata"))
+#save(params.start.values,file=("calibration/starting_values/2025_08_04_nigeria_start_values.Rdata"))
+save(params.start.values,file=("calibration/starting_values/2025_08_13_nigeria_start_values.Rdata"))
