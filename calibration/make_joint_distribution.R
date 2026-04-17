26d9772b096d8e07162983671e5842585ca3d453
mschnure <mschnur3@jhmi.edu>
2026-04-17 08:46:03 -0400

    updated to use for age multipliers

--- calibration/make_joint_distribution.R
+++ calibration/make_joint_distribution.R
@@ -1,16 +1,7 @@
 library(distributions)
 library(boot) 
 
-# PRIOR is set to country-specific prior at the bottom of this file 
-
-# for most of these, log normal is fine; maybe logit normal for some; types of parameters: rates, ratios
-# if lognormal, being off by a factor in either direction is equivalent, rather than absolute value (half versus 2x are the same)
-# log(x)/2 means 95% cred interval from 1/x*guess to x*guess
-
-# mean value given to function is log of what I think the value should be (e.g., mean value of log(1) means I think the value is 1)
-# without data, best guess would have mean at 0 (or 1) and then decide how far off I'm willing to be 
-# log(4)/2 --> can be off by a factor of 4 (=log(2), just keeping this way for clarity)
-
+# updated 4/17/26 to use for age multipliers
 make.joint.distribution = function(median.r2, # anchor on trate.2
                                    sd.r2,
                                    median.r0.to.r1, #0 relative to 1
@@ -20,13 +11,15 @@ make.joint.distribution = function(median.r2, # anchor on trate.2
                                    median.r3.to.r2, #3 relative to 2
                                    sd.r3.to.r2, 
                                    median.r4.to.r3, #4 relative to 3
-                                   sd.r4.to.r3){
+                                   sd.r4.to.r3,
+                                   var.names) # c("trate.0","trate.1","trate.2","trate.3","trate.4")
+{
     
     mean.vector = log(c(median.r2,
-                      median.r0.to.r1,
-                      median.r1.to.r2,
-                      median.r3.to.r2,
-                      median.r4.to.r3))
+                        median.r0.to.r1,
+                        median.r1.to.r2,
+                        median.r3.to.r2,
+                        median.r4.to.r3))
     cov.mat = diag(c(sd.r2,
                      sd.r0.to.r1,
                      sd.r1.to.r2,
@@ -44,9 +37,9 @@ make.joint.distribution = function(median.r2, # anchor on trate.2
     
     Multivariate.Lognormal.Distribution(mu = new.mean.vector,
                                         sigma = new.cov.mat,
-                                        var.names = c("trate.0","trate.1","trate.2","trate.3","trate.4"))
+                                        var.names = var.names)
     
-}
+                                   }
 
 # checking for missing variables
 if(1==2){
